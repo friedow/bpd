2c9ab0153401f87d07f121bdd5dd3ddb7fff35a3
Maximilian <maximilian.voelker@student.hpi.de>
2017-02-10 15:18:16 +0100

    add basic connector for sonarqube

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Overview from './Overview';
 import 'materialize-css/dist/css/materialize.css';
 import 'materialize-css/dist/js/materialize.js';
 import './index.css';
+import SonarqubeConnector from './connectors/SonarqubeConnector.js';
+import RestInterface from './connectors/RestInterface.js';
+
+const sonarqubeClient = new SonarqubeConnector("https://bpt-lab.org/sonarqube");
 
 ReactDOM.render(
-  <App />,
+  <Overview projects={sonarqubeClient.getProjectList()} />,
   document.getElementById('root')
 );
