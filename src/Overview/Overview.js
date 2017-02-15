f30a8381a4861532433cb91156d77b1c185470c2
Christian Friedow <christian@friedow.com>
2017-02-15 11:53:06 +0100

    Refactored overview

--- src/Overview/Overview.js
+++ src/Overview/Overview.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
-import './Overview.css';
 import ProjectLane from './ProjectLane/ProjectLane.js';
+import './Overview.css';
 
 class Overview extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {projectLanes: null};
-  }
-  componentDidMount() {
-    this.loadProjectLanes();
-  }
-  loadProjectLanes() {
-    const projectLanes = this.props.monitoredRepositories.map((repository) =>
-      <ProjectLane key={repository} repository={repository} apiClients={this.props.apiClients} />
-    );
-    this.setState({projectLanes: projectLanes});
-  }
   render() {
     return (
       <div>
         <nav>List of projects</nav>
-        {this.state.projectLanes}
+        {this.props.monitoredRepositories.map((repository) =>
+          <ProjectLane key={repository}
+                       repository={repository}
+                       apiClients={this.props.apiClients} />
+        )}
       </div>
     );
   }
