17e664c2886e8a3c228c21bb0b5bc95e8886a349
Christian Friedow <christian@friedow.com>
2017-02-15 11:50:44 +0100

    Moved imports and removed unnecessary ones

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Overview from './Overview/Overview.js';
+import GitHubConnector from './Connectors/GitHubConnector.js';
+import SonarqubeConnector from './Connectors/SonarqubeConnector.js';
+import TravisConnector from './Connectors/TravisConnector.js';
 import './App.css';
+
+const sonarqubeClient = new SonarqubeConnector('http://localhost:8091/https://bpt-lab.org/sonarqube');
+const travisClient = new TravisConnector('https://api.travis-ci.org');
+const gitHubClient = new GitHubConnector('https://api.github.com');
+const clients = {sonarqube:sonarqubeClient, gitHub:gitHubClient, travis:travisClient};
+const monitoredRepositories = ['bptlab/argos-frontend', 'bptlab/argos-backend'];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+          <Overview monitoredRepositories={monitoredRepositories} apiClients={clients} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Overview from './Overview/Overview.js';
-import 'materialize-css/dist/css/materialize.css';
 import 'materialize-css/dist/js/materialize.js';
+import App from './App.js';
+import 'materialize-css/dist/css/materialize.css';
 import './index.css';
-import GitHubConnector from './Connectors/GitHubConnector.js';
-import SonarqubeConnector from './Connectors/SonarqubeConnector.js';
-import TravisConnector from './Connectors/TravisConnector.js';
-import RestInterface from './Connectors/RestInterface.js';
 
-const sonarqubeClient = new SonarqubeConnector("http://localhost:8091/https://bpt-lab.org/sonarqube");
-const travisClient = new TravisConnector("https://api.travis-ci.org");
-const gitHubClient = new GitHubConnector("https://api.github.com");
-const clients = {sonarqube:sonarqubeClient, gitHub:gitHubClient, travis:travisClient};
-const monitoredRepositories = ['bptlab/argos-frontend', 'bptlab/argos-backend'];
-
-// <Overview projects={sonarqubeClient.getProjectList()} />,
 ReactDOM.render(
-  <Overview monitoredRepositories={monitoredRepositories} apiClients={clients} />,
+  <App/>,
   document.getElementById('root')
 );
