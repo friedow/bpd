a636ab19525b0b9dc6d90973cfe8572424854f80
Maximilian <maximilian.voelker@student.hpi.de>
2017-02-17 18:09:17 +0100

    move api-endpoints to properties-file

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import GitHubConnector from './Connectors/GitHubConnector.js';
 import SonarqubeConnector from './Connectors/SonarqubeConnector.js';
 import TravisConnector from './Connectors/TravisConnector.js';
 import CoverallsConnector from './Connectors/CoverallsConnector.js';
+import Settings from './bpd_properties.js';
 import './App.css';
 
-const sonarqubeClient = new SonarqubeConnector('http://localhost:8091/https://bpt-lab.org/sonarqube');
-const travisClient = new TravisConnector('https://api.travis-ci.org');
-const gitHubClient = new GitHubConnector('https://api.github.com');
-const coverallsClient = new CoverallsConnector('http://localhost:8091/https://coveralls.io');
+const sonarqubeClient = new SonarqubeConnector(Settings["sonarqubeApi"]);
+const travisClient = new TravisConnector(Settings["travisApi"]);
+const gitHubClient = new GitHubConnector(Settings["gitHubApi"]);
+const coverallsClient = new CoverallsConnector(Settings["coverallsApi"]);
 const clients = {sonarqube:sonarqubeClient, gitHub:gitHubClient, travis:travisClient, coveralls:coverallsClient};
 const monitoredRepositories = ['bptlab/argos-frontend', 'bptlab/argos-backend'];
 
