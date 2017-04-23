25ec145e1b9807a625580ab71c14642c8459e29e
MaximilianV <maximilian.voelker@mailbox.org>
2017-04-23 10:06:31 +0200

    fix error in projectlane

--- src/Overview/ProjectLane/ProjectLane.js
+++ src/Overview/ProjectLane/ProjectLane.js
@@ -3,21 +3,21 @@ import BranchCard from './BranchCard/BranchCard.js';
 import Repository from '../../Repository.js';
 
 class ProjectLane extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {branches: []};
-  }
-  componentDidMount() {
-    this.loadBranches();
-    this.timerID = setInterval(
-      () => this.loadBranches(),
-      120000
-    );
-  }
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {branches: []};
         this.repository = new Repository(this.props.repository.split("/")[1], this.props.repository.split("/")[0], this.props.apiClients);
+    }
+    componentDidMount() {
+        this.updateBranches();
+        this.timerID = setInterval(
+            () => this.loadBranches(),
+            120000
+        );
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
 
     updateBranches() {
         let shownBranches = this.repository.getMostRecentBranchesWithDev(6);
@@ -25,25 +25,25 @@ class ProjectLane extends Component {
     }
 
 
-  render() {
-    return (
-      <div className="project-lane">
-        <div className="row">
-          <div className="col s12">
-            <div className="card small grey lighten-3">
-              <div className="card-content white-text">
-                <span className="card-title grey-text text-darken-3">{this.props.repository.split("/")[1]}</span>
-                  {this.state.branches.map((branch, index) =>
-                      <BranchCard key={index} branch={branch}/>
-                  )}
-              </div>
-            </div>
-          </div>
+    render() {
+        return (
+            <div className="project-lane">
+                <div className="row">
+                    <div className="col s12">
+                        <div className="card small grey lighten-3">
+                            <div className="card-content white-text">
+                                <span className="card-title grey-text text-darken-3">{this.props.repository.split("/")[1]}</span>
+                                {this.state.branches.map((branch, index) =>
+                                    <BranchCard key={index} branch={branch}/>
+                                )}
+                            </div>
+                        </div>
+                    </div>
 
-        </div>
-      </div>
-    );
-  }
+                </div>
+            </div>
+        );
+    }
 }
 
 export default ProjectLane;
