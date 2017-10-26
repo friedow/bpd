e40a26fe06d3a67791f3a69c2e8ae5771e4db66b
Christian Friedow <christian@friedow.com>
2017-10-26 15:11:58 +0200

    Fixed concurrent function of cors server

--- scripts/cors.js
+++ scripts/cors.js
@@ -1,7 +1,4 @@
 var cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    removeHeaders: ['cookie', 'cookie2']
-}).listen('8091', 'localhost', function() {
+cors_proxy.createServer().listen('8091', 'localhost', function() {
     console.log('Running CORS Anywhere on localhost:8091');
 });
